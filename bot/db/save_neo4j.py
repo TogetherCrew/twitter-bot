c4e313540cbda82b536d58abe2f668ae835999b7
Mohammad Amin <dadgaramin96@gmail.com>
2023-09-08 12:51:07 +0330

    Added predefined message for neo4j transactions!

--- bot/db/save_neo4j.py
+++ bot/db/save_neo4j.py
@@ -9,7 +9,9 @@ from .user_profile_to_cypher import create_twitter_user_profile_query
 from .tweet_likes_to_cypher import create_query_tweet_likes, create_query_user_likes
 
 
-def save_tweets_in_neo4j(twitter_data: list[dict[str, Any]], message: str = "") -> None:
+def save_tweets_in_neo4j(
+    twitter_data: list[dict[str, Any]], message: str | None = None
+) -> None:
     """
     save tweets data into neo4j
 
@@ -17,9 +19,9 @@ def save_tweets_in_neo4j(twitter_data: list[dict[str, Any]], message: str = "") 
     ------------
     twitter_data : list[dict[str, Any]]
         list of tweet dictionary
-    message : str
+    message : str | None
         optional: additional info on what kind of data is being saved
-        default is empty string
+        default is None
     """
     # connect to neo4j
     neo4j_connection = Neo4jConnection()
@@ -27,10 +29,13 @@ def save_tweets_in_neo4j(twitter_data: list[dict[str, Any]], message: str = "") 
     # create related queries
     queries = create_twitter_data_query(twitter_data)
     # store data into database
+    msg = "saving tweets data into neo4j: "
+    if message is not None:
+        msg += f"{message}: "
     neo4j_ops.store_data_neo4j(query_list=unique(queries), message=message)
 
 
-def save_user_profile_neo4j(user_data: list[User], message: str = "") -> None:
+def save_user_profile_neo4j(user_data: list[User], message: str | None = None) -> None:
     """
     save user profile data into neo4j
 
@@ -38,9 +43,9 @@ def save_user_profile_neo4j(user_data: list[User], message: str = "") -> None:
     ------------
     twitter_data : list[dict[str, Any]]
         list of tweet dictionary
-    message : str
+    message : str | None
         optional: additional info on what kind of data is being saved
-        default is empty string
+        default is None
     """
     # connect to neo4j
     neo4j_connection = Neo4jConnection()
@@ -48,11 +53,14 @@ def save_user_profile_neo4j(user_data: list[User], message: str = "") -> None:
     # create related queries
     queries = create_twitter_user_profile_query(user_data)
     # store data into database
-    neo4j_ops.store_data_neo4j(query_list=unique(queries), message=message)
+    msg = "saving users profile data: "
+    if message is not None:
+        msg += f"{message}: "
+    neo4j_ops.store_data_neo4j(query_list=unique(queries), message=msg)
 
 
 def save_tweet_likes_neo4j(
-    tweet_id: str, users_liked: list[User], message: str = ""
+    tweet_id: str, users_liked: list[User], message: str | None = None
 ) -> None:
     """
     save the likes of a tweet within neo4j
@@ -63,9 +71,9 @@ def save_tweet_likes_neo4j(
         the tweet_id which the logs belong to
     users_liked : list[tweepy.User]
         the list of users liking a tweet
-    message : str
+    message : str | None
         optional: additional info on what kind of data is being saved
-        default is empty string
+        default None
     """
     # connect to neo4j
     neo4j_connection = Neo4jConnection()
@@ -73,11 +81,14 @@ def save_tweet_likes_neo4j(
     # create related queries
     queries = create_query_tweet_likes(tweet_id, users_liked)
     # store data into database
-    neo4j_ops.store_data_neo4j(query_list=unique(queries), message=message)
+    msg = "saving tweets like data: "
+    if message is not None:
+        msg += f"{message}: "
+    neo4j_ops.store_data_neo4j(query_list=unique(queries), message=msg)
 
 
 def save_user_likes_neo4j(
-    user_id: str, tweets_liked: list[Tweet], message: str = ""
+    user_id: str, tweets_liked: list[Tweet], message: str | None = None
 ) -> None:
     """
     save the likes a user did on tweets
@@ -88,9 +99,9 @@ def save_user_likes_neo4j(
         the user_id which did the like of tweets
     tweets_liked : list[tweepy.User]
         the list of tweets liked by the user
-    message : str
+    message : str | None
         optional: additional info on what kind of data is being saved
-        default is empty string
+        default is None
     """
     # connect to neo4j
     neo4j_connection = Neo4jConnection()
@@ -98,4 +109,7 @@ def save_user_likes_neo4j(
     # create related queries
     queries = create_query_user_likes(user_id, tweets_liked)
     # store data into database
-    neo4j_ops.store_data_neo4j(query_list=unique(queries), message=message)
+    msg = "saving users liked tweets data: "
+    if message is not None:
+        msg += f"{message}: "
+    neo4j_ops.store_data_neo4j(query_list=unique(queries), message=msg)
