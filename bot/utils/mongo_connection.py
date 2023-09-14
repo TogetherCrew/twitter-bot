acbbbe270219e0a6823108aaf1f0230f08eb52b9
Mohammad Twin <scientists.twin@gmail.com>
2023-09-14 12:54:36 +0400

    feat: redis connection was added

---
+++ bot/utils/mongo_connection.py
@@ -0,0 +1,54 @@
+import os
+from typing import Any
+
+from dotenv import load_dotenv
+
+
+def get_mongo_credentials():
+    """
+    load mongo db credentials from .env
+
+    Returns:
+    ---------
+    mongo_creds : dict[str, Any]
+        mongodb credentials
+        a dictionary representive of
+            `user`: str
+            `password` : str
+            `host` : str
+            `port` : int
+    """
+    load_dotenv()
+
+    mongo_creds = {}
+
+    mongo_creds["user"] = os.getenv("MONGODB_USER")
+    mongo_creds["password"] = os.getenv("MONGODB_PASS")
+    mongo_creds["host"] = os.getenv("MONGODB_HOST")
+    mongo_creds["port"] = os.getenv("MONGODB_PORT")
+
+    return mongo_creds
+
+def get_mongo_connection(mongo_creds: dict[str, Any] = get_mongo_credentials()):
+    user = mongo_creds["user"]
+    password = mongo_creds["password"]
+    host = mongo_creds["host"]
+    port = mongo_creds["port"]
+
+    connection = f"mongodb://{user}:{password}@{host}:{port}"
+
+    return connection
+
+def get_saga_db_location(mongo_creds: dict[str, Any] = get_mongo_credentials()):
+    """
+    get the saga location in database
+    """
+    load_dotenv()
+
+    saga_db = {}
+
+    saga_db["db_name"] = os.getenv("SAGA_DB_NAME")
+    saga_db["collection_name"] = os.getenv("SAGA_DB_COLLECTION")
+    saga_db["connection_str"] = get_mongo_connection(mongo_creds)
+
+    return saga_db
