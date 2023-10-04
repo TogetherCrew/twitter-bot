51b1291d963bf20faa2d68efa9364cf1f2bb035f
Mohammad Twin <scientists.twin@gmail.com>
2023-10-04 18:21:45 +0400

    feat: return type was added to the function

--- bot/utils/get_epoch.py
+++ bot/utils/get_epoch.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 
 
-def get_x_days_ago_UTC_timestamp(x: int):
+def get_x_days_ago_UTC_timestamp(x: int) -> int:
     x_days_ago_timestamp = int(
         (datetime.now(tz=timezone.utc) - timedelta(days=x)).timestamp() * 1000
     )
