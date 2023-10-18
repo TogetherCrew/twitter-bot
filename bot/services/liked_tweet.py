eb9a7569c8f0c57976534e8f1ec5dc5f9cd61c5c
Cyrille <1618640+cyri113@users.noreply.github.com>
2023-10-18 16:43:43 +0200

    reduce cognitive complexity

--- bot/services/liked_tweet.py
+++ bot/services/liked_tweet.py
@@ -38,26 +38,11 @@ def get_liked_tweets_since(user_id: str, since: int | None = None):
     next_token = None
 
     for _ in count(1):
-        tweets = retry_function_if_fail(
-            TwitterClient.client.get_liked_tweets,
-            id=user_id,
-            tweet_fields=FetchConfigs.tweet_fields,
-            max_results=5,
-            pagination_token=next_token,
-        )
-        tweets_list = tweets.data
-        tweets_meta = tweets.meta
+        tweets, tweets_meta = fetch_liked_tweets(user_id, next_token)
+        all_tweets += tweets
 
-        tweets_list = tweets_list if tweets_list is not None else []
-        all_tweets += tweets_list
-
-        if since:  # check if there is a tweet that was created before the `since`
-            tweets_time = list(
-                map(lambda tweet: tweet.created_at.timestamp() * 1000, tweets_list)
-            )
-            times_before_since = list(filter(lambda time: time < since, tweets_time))
-            if len(times_before_since) > 0:
-                break
+        if since and any(tweet.created_at.timestamp() * 1000 < since for tweet in tweets):
+            break
 
         if "next_token" not in tweets_meta:
             # when we don't have "next_token" in meta object, there is no more data
@@ -92,3 +77,15 @@ def get_likers_of_tweet(tweet_id: str) -> list[tweepy.User]:
             next_token = users_meta["next_token"]
 
     return all_liker_users
+
+def fetch_liked_tweets(user_id, next_token):
+    max_results = 5
+    tweets, tweets_meta = TwitterClient.client.get_liked_tweets(
+        id=user_id,
+        tweet_fields=FetchConfigs.tweet_fields,
+        max_results=max_results,
+        pagination_token=next_token
+    )
+    
+    tweets_list = tweets.data or []
+    return tweets_list, tweets_meta
