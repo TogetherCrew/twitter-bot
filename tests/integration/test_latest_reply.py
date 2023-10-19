594c21b6f58da534f7060623d8bb7a7af796763c
Mohammad Amin <dadgaramin96@gmail.com>
2023-10-19 19:45:24 +0330

    fix: fixed the tests with the latest updates! the code was updated in other branches before but the tests wasn't updated. so now we include the update for them.

--- tests/integration/test_latest_reply.py
+++ tests/integration/test_latest_reply.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from bot.db.latest_reply import get_latest_reply_since
 from bot.db.neo4j_connection import Neo4jConnection
 
@@ -71,20 +73,28 @@ def test_get_latest_reply_userid_input():
         MATCH (n) DETACH DELETE (n)
         """
     )
+    created_time = int((datetime.now() - timedelta(days=2)).timestamp() * 1000)
 
     neo4j_ops.gds.run_cypher(
-        """
-        MERGE (a:Tweet {tweetId: '1111', authorId: '989898'})
-        MERGE (b:Tweet {tweetId: '1112', authorId: '989899'})
-        MERGE (c:Tweet {tweetId: '1113', authorId: '9898100'})
-        MERGE (d:Tweet {tweetId: '1114', authorId: '989898'})
-        MERGE (e:Tweet {tweetId: '1115', authorId: '989897'})
+        f"""
+        MERGE (a:Tweet {{tweetId: '1111', authorId: '989898'}})
+        MERGE (b:Tweet {{tweetId: '1112', authorId: '989899'}})
+        MERGE (c:Tweet {{tweetId: '1113', authorId: '9898100'}})
+        MERGE (d:Tweet {{tweetId: '1114', authorId: '989898'}})
+        MERGE (e:Tweet {{tweetId: '1115', authorId: '989897'}})
 
-        MERGE (a)-[:REPLIED]->(b)
-        MERGE (d)-[:REPLIED]->(a)
-        MERGE (e)-[:REPLIED]->(a)
-        MERGE (d)-[:REPLIED]->(b)
-        MERGE (c)-[:REPLIED]->(d)
+        MERGE (a)-[r:REPLIED]->(b)
+        MERGE (d)-[r2:REPLIED]->(a)
+        MERGE (e)-[r3:REPLIED]->(a)
+        MERGE (d)-[r4:REPLIED]->(b)
+        MERGE (c)-[r5:REPLIED]->(d)
+
+
+        SET r.createdAt = {created_time}
+        SET r2.createdAt = {created_time}
+        SET r3.createdAt = {created_time}
+        SET r4.createdAt = {created_time}
+        SET r5.createdAt = {created_time}
         """
     )
 
@@ -107,19 +117,26 @@ def test_get_latest_reply_tweetid_input():
         """
     )
 
+    created_time = int((datetime.now() - timedelta(days=2)).timestamp() * 1000)
     neo4j_ops.gds.run_cypher(
-        """
-        MERGE (a:Tweet {tweetId: '1111', authorId: '989898'})
-        MERGE (b:Tweet {tweetId: '1112', authorId: '989899'})
-        MERGE (c:Tweet {tweetId: '1113', authorId: '9898100'})
-        MERGE (d:Tweet {tweetId: '1114', authorId: '989898'})
-        MERGE (e:Tweet {tweetId: '1115', authorId: '989897'})
+        f"""
+        MERGE (a:Tweet {{tweetId: '1111', authorId: '989898'}})
+        MERGE (b:Tweet {{tweetId: '1112', authorId: '989899'}})
+        MERGE (c:Tweet {{tweetId: '1113', authorId: '9898100'}})
+        MERGE (d:Tweet {{tweetId: '1114', authorId: '989898'}})
+        MERGE (e:Tweet {{tweetId: '1115', authorId: '989897'}})
 
-        MERGE (a)-[:REPLIED]->(b)
-        MERGE (c)-[:REPLIED]->(a)
-        MERGE (d)-[:REPLIED]->(a)
-        MERGE (e)-[:REPLIED]->(a)
-        MERGE (c)-[:REPLIED]->(d)
+        MERGE (a)-[r:REPLIED]->(b)
+        MERGE (c)-[r2:REPLIED]->(a)
+        MERGE (d)-[r3:REPLIED]->(a)
+        MERGE (e)-[r4:REPLIED]->(a)
+        MERGE (c)-[r5:REPLIED]->(d)
+
+        SET r.createdAt = {created_time}
+        SET r2.createdAt = {created_time}
+        SET r3.createdAt = {created_time}
+        SET r4.createdAt = {created_time}
+        SET r5.createdAt = {created_time}
         """
     )
 
